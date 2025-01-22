76e82696edc6cd43e57eeebaab86ff3650a5040a
Bhanuprakash842 <127642353+Bhanuprakash842@users.noreply.github.com>
2025-01-22 20:07:55 +0530

    Update Userprofile and picture

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,8 @@
-import { text } from "express";
+import { uploadPicture } from "../middleware/uploadPictureMiddleware";
 import User from "../models/User";
+import { fileRemover } from "../utils/fileRemover";
 
-export const registerUser = async (req, res,next) => {
+const registerUser = async (req, res, next) => {
   try {
     const { name, email, password } = req.body;
 
@@ -33,16 +34,17 @@ export const registerUser = async (req, res,next) => {
   }
 };
 
-const loginUser = async (req,res, next) => {
-  try{
-    const {email,password} = req.body;
+const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
 
-    let user = await User.findOne({email});
-    if(!user){
+    let user = await User.findOne({ email });
+
+    if (!user) {
       throw new Error("Email not found");
     }
 
-    if(await user.comparePassword(password)){
+    if (await user.comparePassword(password)) {
       return res.status(201).json({
         _id: user._id,
         avatar: user.avatar,
@@ -52,12 +54,10 @@ const loginUser = async (req,res, next) => {
         admin: user.admin,
         token: await user.generateJWT(),
       });
-    }
-    else {
+    } else {
       throw new Error("Invalid email or password");
     }
-  }
-  catch(error){
+  } catch (error) {
     next(error);
   }
 };
@@ -85,5 +85,93 @@ const userProfile = async (req, res, next) => {
   }
 };
 
+const updateProfile = async (req, res, next) => {
+  try {
+    let user = await User.findById(req.user._id);
 
-export { registerUser, loginUser,userProfile };
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password && req.body.password.length < 6) {
+      throw new Error("Password length must be at least 6 character");
+    } else if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUserProfile = await user.save();
+
+    res.json({
+      _id: updatedUserProfile._id,
+      avatar: updatedUserProfile.avatar,
+      name: updatedUserProfile.name,
+      email: updatedUserProfile.email,
+      verified: updatedUserProfile.verified,
+      admin: updatedUserProfile.admin,
+      token: await updatedUserProfile.generateJWT(),
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateProfilePicture = async (req, res, next) => {
+  try {
+    const upload = uploadPicture.single("profilePicture");
+
+    upload(req, res, async function (err) {
+      if (err) {
+        const error = new Error("An unknown error occured when uploading " + err.message);
+        next(error);
+      } else {
+        // every thing went well
+        if (req.file) {
+          const updatedUser = await User.findByIdAndUpdate(
+            req.user._id,
+            {
+              avatar: req.file.filename,
+            },
+            { new: true }
+          );
+          res.json({
+            _id: updatedUser._id,
+            avatar: updatedUser.avatar,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            verified: updatedUser.verified,
+            admin: updatedUser.admin,
+            token: await updatedUser.generateJWT(),
+          });
+        } else {
+          let filename;
+          let updatedUser = await User.findById(req.user._id);
+          filename = updatedUser.avatar;
+          updatedUser.avatar = "";
+          await updatedUser.save();
+          fileRemover(filename);
+          res.json({
+            _id: updatedUser._id,
+            avatar: updatedUser.avatar,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            verified: updatedUser.verified,
+            admin: updatedUser.admin,
+            token: await updatedUser.generateJWT(),
+          });
+        }
+      }
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export {
+  registerUser,
+  loginUser,
+  userProfile,
+  updateProfile,
+  updateProfilePicture,
+};
